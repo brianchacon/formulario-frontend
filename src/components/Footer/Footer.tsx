99403f735dcf29860438b06d74f8de9b5d8e1612
Brian <brian89.chacon89@gmail.com>
2024-05-08 17:16:30 -0300

    Dando forma inicial a footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+function Footer() {
+    return (
+      <div className='footer-container'>
+          <div className='footer-elem'>
+              <div>
+                  <a href="/">
+                    <h3>CapacitAI</h3>
+                  </a> 
+              </div>
+          </div>       
+      </div>
+    )
+  }
+  export default Footer
